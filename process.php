73e0a6ddf810a983d7cc4b50e7a8d11d51014ba2
edgarsingui <edgarsingui@gmail.com>
2021-10-19 09:34:39 +0100

    first commit

---
+++ process.php
@@ -0,0 +1,51 @@
+<?php
+$subjectPrefix = '[Email via website]';
+$emailTo       = 'ola@bussola.ao';
+$errors = array(); 
+$data   = array(); 
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name    = stripslashes(trim($_POST['name']));
+    $email   = stripslashes(trim($_POST['email']));
+    $subject = stripslashes(trim($_POST['subject']));
+    $message = stripslashes(trim($_POST['message']));
+    if (empty($name)) {
+        $errors['name'] = 'Insira o nome.';
+    }
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = 'Insira o e-mail.';
+    }
+    if (empty($subject)) {
+        $errors['subject'] = 'Insira um assunto.';
+    }
+    if (empty($message)) {
+        $errors['message'] = 'Insira a mensagem.';
+    }
+    if (!empty($errors)) {
+        $data['success'] = false;
+        $data['errors']  = $errors;
+    } else {
+        $subject = "$subjectPrefix $subject";
+        $body    = '
+            <strong>Nome: </strong>'.$name.'<br />
+            <strong>Email: </strong>'.$email.'<br />
+            <strong>Mensagem: </strong>'.nl2br($message).'<br />
+        ';
+        $headers  = "MIME-Version: 1.1" . PHP_EOL;
+        $headers .= "Content-type: text/html; charset=utf-8" . PHP_EOL;
+        $headers .= "Content-Transfer-Encoding: 8bit" . PHP_EOL;
+        $headers .= "Date: " . date('r', $_SERVER['REQUEST_TIME']) . PHP_EOL;
+        $headers .= "Message-ID: <" . $_SERVER['REQUEST_TIME'] . md5($_SERVER['REQUEST_TIME']) . '@' . $_SERVER['SERVER_NAME'] . '>' . PHP_EOL;
+        $headers .= "From: " . "=?UTF-8?B?".base64_encode($name)."?=" . "<$email>" . PHP_EOL;
+        $headers .= "Return-Path: $emailTo" . PHP_EOL;
+        $headers .= "Reply-To: $email" . PHP_EOL;
+        $headers .= "X-Mailer: PHP/". phpversion() . PHP_EOL;
+        $headers .= "X-Originating-IP: " . $_SERVER['SERVER_ADDR'] . PHP_EOL;
+        mail($emailTo, "=?utf-8?B?" . base64_encode($subject) . "?=", $body, $headers);
+        $data['success'] = true;
+        $data['message'] = 'Sua mensagem foi enviada';
+    }
+    echo json_encode($data);
+}
+
+
+
